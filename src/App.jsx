f88092127728ec31a1d0d9e4947a4a0751444398
hennanedouaa <ld_hennane@esi.dz>
2024-08-22 14:32:52 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-green-600 h-screen w-screen'>
+      {/* The background will fill the entire viewport */}
+    </div>
+  )
+}
+
+export default App
+
