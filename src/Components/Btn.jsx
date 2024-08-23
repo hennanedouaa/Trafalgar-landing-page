73259f5b67a6296cad7a71450a0b3fc2126830c4
hennanedouaa <ld_hennane@esi.dz>
2024-08-23 13:02:51 +0100

    the white Btn

---
+++ src/Components/Btn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+    export default function Btn(props){
+        return(
+        <button className="bg-white border border-lblue self-center lg:self-start text-lg lg:text-[26px] text-lblue font-medium px-14 py-4 lg:px-14 lg:py-6 rounded-full lg:mt-8 hover:scale-105">
+           {props.textee}
+        </button>
+        )
+    }
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import Btn from "./Btn";
 import search from "../assets/images/search.svg";
 import pills from "../assets/images/pills.svg";
 import consultasion from "../assets/images/consultasion.svg";
@@ -17,7 +18,7 @@ export default function Services(){
                     <hr className="border-b-[1px] border-black w-16 self-center mt-6"/>
                     <p className="text-gray-400 lg:mt-4 lg:text-xl mx-8 my-6">We provide to you the best choices for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors. You can consult with us to find the most suitable service for your health.</p>
                 </div>
-                <div className="grid bg-transparent gap-x-14 gap-y-10  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
+                <div className="grid bg-transparent gap-x-14 gap-y-10  grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 lg:mb-4">
                     <Card image={search} titre="Search doctor" textee="Choose your doctor from thousands of specialist, general, and trusted hospitals" />
                     <Card image={pills} titre="Online pharmacy" textee="Buy your medicines with our mobile application with a simple delivery system" />
                     <Card image={consultasion} titre="Consultation" textee="Free consultation with our trusted doctors and get the best recomendations" />
@@ -25,6 +26,7 @@ export default function Services(){
                     <Card image={emergency} titre="Emergency care" textee="You can get 24/7 urgent care for yourself or your children and your lovely family" />
                     <Card image={tracking} titre="Tracking" textee="Track and save your medical history and health dat" />
                 </div>
+                <Btn textee="Learn More" />
             </div>
         </div>
     );
